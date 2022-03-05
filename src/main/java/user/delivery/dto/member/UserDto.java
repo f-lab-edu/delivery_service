705056a6343c3a13729fba0adb6d3af89a4676f7
skskskdd22 <skskskdd22@naver.com>
2022-03-05 12:12:35 +0900

    delivery_service service 추가중

--- src/main/java/user/delivery/dto/member/UserDto.java
+++ src/main/java/user/delivery/dto/member/UserDto.java
@@ -1,38 +1,53 @@
 package user.delivery.dto.member;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class UserDto {
 
-    public enum Type{
+    public enum Type {
         PARTNER, USER
     }
 
-    @NonNull
+    @NotEmpty(message = "id를 적어주세요.")
     private String id;
 
-    @NonNull
+
+    @NotEmpty(message = "비밀번호를 적어주세요.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$\n",
+            message = "10자 이상 영어 대문자, 소문자, 숫자 , 특수문자 중 2종류를 조합이여야 합니다.")
     private String password;
 
-    @NonNull
+
+    @NotEmpty(message = "닉네임를 적어주세요.")
+    @Size(min = 2, max = 8)
     private String nickname;
 
-    @NonNull
+
+    @NotEmpty(message = "휴대폰번호를 적어주세요.")
     private String phone;
 
-    @NonNull
+
+    @NotEmpty(message = "이메일을 적어주세요.")
     private String email;
 
-    @NonNull
+
+    @NotEmpty(message = "주소를 적어주세요.")
     private String address;
 
-    @NonNull
+
+    @NotEmpty(message = "회원가입 유형을 선택해주세요.")
     private Type type;
 
 }
---
+++ src/main/java/user/delivery/repository/UserRepository.java
@@ -0,0 +1,27 @@
+package user.delivery.repository;
+
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import user.delivery.dto.member.UserDto;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository {
+
+    boolean join(UserDto user);
+
+    boolean checkLogIn(@Param("id") String id, @Param("password") String password);
+
+    UserDto updatePassword(String id, String password);
+
+    void updateUser(@Param("id") String id, @Param("password") String password, @Param("phone")String phone);
+
+    void deleteUser(String id);
+
+    boolean findById(String phone);
+
+    UserDto findByUser(String id);
+
+    UserDto findByEmail(String email);
+}
